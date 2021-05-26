41e71258f90b757f3fe9799b2ed30408ffb47bc5
Jungwoo-20 <jungwoo7250@naver.com>
2021-05-26 23:50:47 +0900

    Join

--- src/test/java/jpabook/jpashop/querydsl/QueryDSLTest.java
+++ src/test/java/jpabook/jpashop/querydsl/QueryDSLTest.java
@@ -6,11 +6,14 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.domain.item.QItem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
@@ -113,7 +116,7 @@ public class QueryDSLTest {
     }
 
     @Test
-    public void 그() throws Exception {
+    public void 그룹() throws Exception {
         JPAQueryFactory query = new JPAQueryFactory(em);
         QItem item = QItem.item;
         List<Tuple> counts = query.selectFrom(item)
@@ -139,5 +142,49 @@ public class QueryDSLTest {
                 .from(member)
                 .fetch();
         memberDTOs.stream().forEach(dto -> System.out.println("dto.getName() = " + dto.getUsername()));
+    }
+
+    @Test
+    public void 다대일조인() throws Exception {
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QMember member = QMember.member;
+        QOrder order = QOrder.order;
+
+        // N+1문제가 있음
+//        List<Order> orderWithMember = query.selectFrom(order)
+//                .join(order.member, member)
+//                .fetch(); //Order와 Member를 조인해도 따로 불러냄 --> Lazy로 설정했기 때문
+//        orderWithMember
+//                .stream()
+//                .forEach(o -> System.out.println("o.getMember().getName() = " + o.getMember().getName()));
+        List<Order> orderWithMember = em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+        orderWithMember.stream()
+                .forEach(o -> System.out.println("o.getMember().getName() = " + o.getOrderItems().get(0).getItem().getName()));
+    }
+
+    @Test
+    public void 일대다조인() throws Exception {
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QMember member = QMember.member;
+        QOrder order = QOrder.order;
+
+//        List<Member> resultList = em.createQuery(
+//                "select m from Member m" + " join fetch m.orders o", Member.class)
+//                .setFirstResult(0)
+//                .setMaxResults(1)
+//                .getResultList();
+        // 일대다관계는 패치로 조인하는 것이 아니라 대상 엔티티만 조회한다. 배치
+        List<Member> resultList = em.createQuery(
+                "select m from Member m", Member.class)
+                .setFirstResult(0)
+                .setMaxResults(2)
+                .getResultList();
+        resultList.stream()
+                .forEach(m -> m.getOrders().stream().forEach(o -> System.out.println("o.getOrderDate() = " + o.getOrderDate())));
     }
 }
